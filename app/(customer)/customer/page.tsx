8b0673caa42adad22bb0e6ef772280d5b1d21776
aditya kumar <aaditya1392@gmail.com>
2025-03-16 14:05:10 +0530

    created transaction page for customer

--- app/(customer)/customer/page.tsx
+++ app/(customer)/customer/page.tsx
@@ -1,12 +1,102 @@
 import { auth } from "@/lib/auth";
-
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { format } from "date-fns";
+import { IndianRupee } from "lucide-react";
 export default async function CustomerDashboard() {
     const session = await auth();
 
+    if (!session || session.user.userType != "customer") {
+        redirect("/");
+    }
+
+    const [recentTransactions, wallet] = await prisma.$transaction([
+        prisma.transaction.findMany({
+            where: {
+                walletId: session?.user?.customer?.wallet?.id,
+            },
+            take: 10,
+            orderBy: {
+                createBy: "desc",
+            },
+            include: {
+                staff: {
+                    select: {
+                        name: true,
+                        id: true,
+                    },
+                },
+            },
+        }),
+        prisma.wallet.findUnique({
+            where: {
+                customerId: session?.user?.customer?.id,
+            },
+        }),
+    ]);
     return (
-        <div>
-            <h1>Customer Dashboard</h1>
-            <p>{session?.user?.customer?.name}</p>
+        <div className="flex flex-col gap-4  max-w-customHaf lg:max-w-custom mx-auto py-5">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-xl">Current Balance</CardTitle>
+                </CardHeader>
+
+                <CardContent>
+                    <div className="flex items-center text-2xl font-bold">
+                        <IndianRupee className="w-5 h-5" />
+                        <span>{wallet?.balance}</span>
+                    </div>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Recent Transactions</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>T.No</TableHead>
+                                <TableHead>Date</TableHead>
+                                <TableHead>Amount</TableHead>
+                                <TableHead>Type</TableHead>
+                                <TableHead>By</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {recentTransactions.map((transaction) => (
+                                <TableRow key={transaction.id}>
+                                    <TableCell>{transaction.id}</TableCell>
+                                    <TableCell>
+                                        {format(
+                                            transaction.createdAt,
+                                            "dd/MM/yyyy",
+                                        )}
+                                    </TableCell>
+                                    <TableCell className="flex items-center gap-1">
+                                        <IndianRupee className="h-3 w-3" />{" "}
+                                        {transaction.amount}
+                                    </TableCell>
+                                    <TableCell>{transaction.type}</TableCell>
+                                    <TableCell className="whitespace-nowrap">
+                                        {transaction.staff?.name} (
+                                        {transaction.staff?.id})
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
         </div>
     );
 }
